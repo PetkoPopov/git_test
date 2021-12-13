49d06fd6490e174219c38f509891904d4e889805
pipjivko <pipjivko@gmail.com>
2021-12-13 20:32:33 +0200

    'save'

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="myCss.css"/>
     </head>
     <body>
         <?php
